e03f664f553082377b5cdc455168aa8f3975615e
Singh <gsingh@BAN15365.Dlink>
2020-10-17 14:23:15 +0530

    first commit

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+import tokenizers
+
+MAX_LEN = 128
+TRAIN_BATCH_SIZE = 32
+VALID_BATCH_SIZE = 16
+EPOCHS = 10
+BERT_PATH = "/Users/gsingh/Documents/Personnal/Projects/Bert_Sentiment_IMDB/input/bert-base-uncased"
+MODEL_PATH = "model.bin"
+TRAINING_FILE = "../input/train.csv"
+TOKENIZER = tokenizers.BertWordPieceTokenizer(
+    os.path.join(BERT_PATH, 'vocab.txt'),
+    lowercase=True
+)
+
